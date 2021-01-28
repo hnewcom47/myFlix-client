f89f2f748d5f62a6c697863333ea738e0ec0f80b
hnewcom47 <71475246+hnewcom47@users.noreply.github.com>
2021-01-28 11:00:10 -0600

    Updated MainView, MovieCard and MovieView components

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+import './main-view.scss';
+
+export class MainView extends React.Component {
+    constructor() {
+        // Call the superclass constructor so React can initialize it
+        super();
+        // Initial state is set to null
+        this.state = {
+            movies: null,
+            selectedMovie: null,
+            user: null,
+            email: null
+        };
+    }
+
+    componentDidMount() {
+        axios.get('https://movies4you-application.herokuapp.com/movies')
+            .then(response => {
+                // Assign the result to the state
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    // When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` property to that movie
+    onMovieClick(movie) {
+        this.setState({
+            selectedMovie: movie
+        });
+    }
+
+    // When back button on movie view clicked, set state to null and go back to movie list 
+    goBack() {
+        this.setState({
+            selectedMovie: null
+        });
+    }
+
+    // When a user successfully logs in, this function updates the `user` property in state to that particular user
+    onLoggedIn(user) {
+        this.setState({
+            user
+        });
+    }
+
+    onRegistration(email) {
+        this.setState({
+            email
+        });
+    }
+
+    // This overrides the render() method of the superclass
+    render() {
+        // If the state isn't initialized, this will throw on runtime before the data is initially loaded
+        const { movies, selectedMovie, user, email } = this.state;
+
+        if (!email) return <RegistrationView onRegistration={email => this.onRegistration(email)} />;
+
+        // If there is no user, the LoginView is rendered.
+        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+        // Before the movies have been loaded
+        if (!movies) return <div className="main-view" />;
+
+        return (
+            // If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all movies will be returned
+            <div className="main-container">
+                <div className="main-view">
+                    {selectedMovie
+                        ? <MovieView movie={selectedMovie} goBack={() => this.goBack()} />
+                        : movies.map(movie => (
+                            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+                        ))
+                    }
+                </div>
+            </div>
+        );
+    }
+}
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { Button } from 'react-bootstrap';
+
+import './movie-view.scss';
+
 export class MovieView extends React.Component {
     constructor() {
         super();
@@ -7,7 +11,7 @@ export class MovieView extends React.Component {
     }
 
     render() {
-        const { movie } = this.props;
+        const { movie, goBack } = this.props;
 
         if (!movie) return null;
 
@@ -15,29 +19,29 @@ export class MovieView extends React.Component {
             <div className="movie-view">
 
                 <img className="movie-poster" src={movie.ImagePath} />
-
+                <br />
                 <div className="movie-title">
                     <span className="label">Title: </span>
                     <span className="value">{movie.Title}</span>
                 </div>
-
+                <br />
                 <div className="movie-description">
                     <span className="label">Description: </span>
                     <span className="value">{movie.Description}</span>
                 </div>
-
+                <br />
                 <div className="movie-genre">
                     <span className="label">Genre: </span>
-                    <span className="value">{movie.Genre.Name}</span>
+                    <Button variant="link">{movie.Genre.Name}</Button>
                 </div>
-
+                <br />
                 <div className="movie-director">
                     <span className="label">Director: </span>
-                    <span className="value">{movie.Director.Name}</span>
+                    <Button variant="link">{movie.Director.Name}</Button>
                 </div>
-
-                <button onClick={this.refreshPage}>Go Back</button>
-
+                <br />
+                <Button onClick={() => goBack()} variant="outline-danger">Back</Button>
+                <Button variant="primary" type="submit">Add to Favorites!</Button>
             </div>
         );
     }

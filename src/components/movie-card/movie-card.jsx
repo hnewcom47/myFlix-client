f3b49b9d2894c0b029e09a00effa92ee3140df46
hnewcom47 <71475246+hnewcom47@users.noreply.github.com>
2021-02-05 12:02:22 -0600

    Updated movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,45 +1,114 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
+import { Link } from 'react-router-dom';
 import { Button, Card } from 'react-bootstrap';
 
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        let addFavorite = false;
+        if (props.addFavorite) {
+            addFavorite = true;
+        }
+
+        let removeFavorite = false;
+        if (props.removeFavorite) {
+            removeFavorite = true;
+        }
+
+        this.state = {
+            movie: this.props.movie,
+            username: props.user,
+            userToken: props.userToken,
+            addFavorite: addFavorite,
+            removeFavorite: removeFavorite,
+        };
+    }
+
+    addFavorite = () => {
+        this.setState({
+            addFavorite: false,
+        });
+        axios({
+            method: 'post',
+            url: `https://movies4you-application.herokuapp.com/users/${this.state.username}/Movies/${this.state.movie._id}`,
+            headers: { Authorization: `Bearer ${this.state.userToken}` },
+            data: {},
+        })
+            .then((response) => {
+                console.log('movie added');
+            })
+            .catch((e) => {
+                console.log(e);
+                console.log('movie not added');
+            });
+    };
+
+    removeFavorite = () => {
+        this.props.updateFavorites(this.state.movie._id);
+        this.setState({
+            removeFavorite: false,
+        });
+        axios.delete(`https://movies4you-application.herokuapp.com/users/${this.state.username}/Movies/${this.state.movie._id}`, {
+            headers: { Authorization: `Bearer ${this.state.userToken}` },
+        })
+            .then((response) => {
+                console.log('movie removed');
+            })
+            .catch((e) => {
+                console.log(e);
+                console.log('could not remove favorite');
+            });
+    };
+
     render() {
-        /* This is given to the <MovieCard/> component by the outer world which, in this case, is `MainView`, 
-        as `MainView` is what’s connected to your database via the movies endpoint of your API */
-        const { movie, onClick } = this.props;
+
+        const { movie } = this.props;
 
         return (
             <Card>
                 <Card.Img variant="top" src={movie.ImagePath} />
                 <Card.Body className="bg-dark">
-                    <table className="main_div">
-                        <tbody>
-                            <tr>
-                                <td valign="top">
-                                    <Card.Title>{movie.Title}</Card.Title>
-                                    <Card.Text>{movie.Description}</Card.Text>
-                                </td>
-                            </tr>
-                            <tr valign="bottom" className="button-wrapper">
-                                <td>
-                                    <Button className="more-button" variant="primary" onClick={() => onClick(movie)}>
-                                        More
-                                        </Button>
+                    <Card.Title>{movie.Title}</Card.Title>
+                    <Card.Text>{movie.Description}</Card.Text>
+                    <div className="button-wrapper">
+                        <Link to={`/movies/${movie._id}`}>
+                            <Button
+                                className="more-button"
+                                variant="primary"
+                            >
+                                More
+                            </Button>
+                        </Link>
 
-                                    <Button className="favorite-button" variant="secondary" type="submit">
-                                        Add to Favorites!
-                                        </Button>
+                        {this.state.addFavorite && (
+                            <Button
+                                className="favorite-button"
+                                variant="secondary"
+                                type="submit"
+                                onClick={this.addFavorite}
+                            >
+                                Add Favorite
+                            </Button>
+                        )}
 
-                                    <Button className="remove-button" variant="outline-danger" type="submit">
-                                        Remove Favorite
-                                        </Button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                        {this.state.removeFavorite && (
+                            <Button
+                                className="remove-button"
+                                variant="outline-danger"
+                                type="submit"
+                                onClick={this.removeFavorite}
+                            >
+                                Remove Favorite
+                            </Button>
+                        )}
+                    </div>
                 </Card.Body>
             </Card>
         );
@@ -50,18 +119,12 @@ MovieCard.propTypes = {
     movie: PropTypes.shape({
         Title: PropTypes.string.isRequired,
         Description: PropTypes.string.isRequired,
-        ImagePath: PropTypes.string.isRequired,
         Genre: PropTypes.shape({
-            Name: PropTypes.string.isRequired,
-            Description: PropTypes.string.isRequired
-        }),
+            Name: PropTypes.string.isRequired
+        }).isRequired,
         Director: PropTypes.shape({
-            Name: PropTypes.string.isRequired,
-            Bio: PropTypes.string.isRequired,
-            Birth: PropTypes.string.isRequired,
-            Death: PropTypes.string
-        }),
-        Featured: PropTypes.bool.isRequired
-    }).isRequired,
-    onClick: PropTypes.func.isRequired
+            Name: PropTypes.string.isRequired
+        }).isRequired,
+        ImagePath: PropTypes.string.isRequired,
+    }).isRequired
 }; 

48580c3e7973fb10de656b3af92a455c8bfcf959
hnewcom47 <71475246+hnewcom47@users.noreply.github.com>
2021-02-04 21:46:10 -0600

    Updated dist files

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -48483,7 +48483,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56358" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50274" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
